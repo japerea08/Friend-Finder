8aadcc1b884ad3f302ade533ce849ef1d99a7de3
Joey Perea <j.perea08@gmail.com>
2018-04-24 20:07:01 -0400

    modified server.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 //variables for the server
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
